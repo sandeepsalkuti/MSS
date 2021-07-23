6baa157c7d42d6177ca8173262963643a6d568df
vishalbabburi <vishalbabburi@gmail.com>
2021-07-23 13:33:23 -0500

    updated employee view

--- src/components/userdata/EmployeeData/EmployeeData.js
+++ src/components/userdata/EmployeeData/EmployeeData.js
@@ -1,5 +1,6 @@
 import React, { useState,useEffect,useContext } from "react";
 import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom"; 
 import axios from "axios";
 import "font-awesome/css/font-awesome.min.css";
 import AppContext from "../../AppContext"
@@ -16,6 +17,7 @@ const EmployeeData = () => {
 
       axios(`http://localhost:8022/findStatusByid/${loginresponse.id}`)
       .then(response => {
+        console.log(response.data);
         setData(response.data)
       })
       .catch(error => {
@@ -26,40 +28,87 @@ const EmployeeData = () => {
         setLoading(false);
       })
     }, []);
-
+    const [users, setUser] = useState([]);
   return (
-    <div>
+    // <div>
       
-      <h3>Employee Data</h3>
-      <div>
-        <div>
-            <p>Name:</p>
-        </div>
-        <div>
-        <div style={{backgroundColor:"#f0f0f0", paddingTop:"5px"}}>
-            {data && data.map((entry) =>{
-                return (
-                  <div class="row" style={{ backgroundColor: "white", border: "2px solid black" }}>
-                    <div class="col-sm-2" style={{borderRight: "2px solid black"}}>
-                      <p style={{fontSize: "18px", padding: "3px"}}>{entry.date}</p>
-                    </div>
+    //   <h3>Employee Data</h3>
+    //   <div>
+    //     <div>
+    //         <p>Name:</p>
+    //     </div>
+    //     <div>
+    //     <div style={{backgroundColor:"#f0f0f0", paddingTop:"5px"}}>
+    //         {data && data.map((entry) =>{
+    //             return (
+    //               <div class="row" style={{ backgroundColor: "white", border: "2px solid black" }}>
+    //                 <div class="col-sm-2" style={{borderRight: "2px solid black"}}>
+    //                   <p style={{fontSize: "18px", padding: "3px"}}>{entry.date}</p>
+    //                 </div>
                     
-                    <div class= "col-sm-8" style={{borderRight: "2px solid black"}}>
-                      <p style={{fontSize: "18px", padding: "3px"}}>{entry.task}</p>
-                    </div>
+    //                 <div class= "col-sm-8" style={{borderRight: "2px solid black"}}>
+    //                   <p style={{fontSize: "18px", padding: "3px"}}>{entry.task}</p>
+    //                 </div>
 
-                    <div class= "col-sm-2">
-                      <p style={{fontSize: "18px", padding: "3px"}}>{entry.status}</p>
-                    </div>
-                  </div>
+    //                 <div class= "col-sm-2">
+    //                   <p style={{fontSize: "18px", padding: "3px"}}>{entry.status}</p>
+    //                 </div>
+    //               </div>
                     
-                );
-              })}
-            </div>
-        </div>
-      </div>
+    //             );
+    //           })}
+    //         </div>
+    //     </div>
+    //   </div>
     
       
+    // </div>
+    <div>
+      
+    <div className="container">
+      <div className="py-4">
+        <h1>Your activities</h1> 
+        <table class="table border shadow">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Date</th>
+              <th scope="col">Task</th>
+              <th scope="col">Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+          {data && data.map((entry,index) =>{
+        return(
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{entry.date}</td>
+                <td>{entry.task}</td>
+                <td>{entry.status}</td>
+                <td>
+                  <Link class="btn btn-primary mr-2" to={""}>
+                    View
+                  </Link>
+                  <Link
+                    class="btn btn-outline-primary mr-2"
+                    to={""}
+                  >
+                    Edit
+                  </Link>
+                  <Link
+                    class="btn btn-danger"
+                    //onClick={() => deleteUser(user.id)}
+                  >
+                    Delete
+                  </Link>
+                </td>
+              </tr>
+           );})}
+          </tbody>
+        </table>
+      </div>
+    </div>
     </div>
   );
 };
